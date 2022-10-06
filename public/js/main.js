8a29599786c0198c8d9ff1d28bc46cdb933f9209
Cesar <camongez28@gmail.com>
2022-10-05 22:02:50 -0300

    recuperando rama

--- public/js/main.js
+++ public/js/main.js
@@ -2,26 +2,31 @@ const socket = io.connect();
 
 //------------------------------------------------------------------------------------
 
-const formAgregarProducto = document.getElementById('formAgregarProducto');
-formAgregarProducto.addEventListener('submit', (e) => {
+const formAgregarProducto = document.getElementById("formAgregarProducto");
+formAgregarProducto.addEventListener("submit", (e) => {
   e.preventDefault();
-  const producto = {
-    title: formAgregarProducto[0].value,
-    price: formAgregarProducto[1].value,
-    thumbnail: formAgregarProducto[2].value,
-  };
-  socket.emit('update', producto);
+  //Armar objeto producto y emitir mensaje a evento update
+  // Creo el objeto que voy a enviar por socket
+  const title = document.getElementById("nombre").value;
+  const price = parseFloat(document.getElementById("precio").value);
+  const thumbnail = document.getElementById("foto").value;
+  const product = { title, price, thumbnail };
+  // envio por socket
+  socket.emit("newProduct", product);
+  // resetteo el formulario para que aparezca vacio
   formAgregarProducto.reset();
 });
 
-socket.on('productos', (productos) => {
-  makeHtmlTable(productos).then((html) => {
-    document.getElementById('productos').innerHTML = html;
-  });
+socket.on("productos", async (productos) => {
+  //generar el html y colocarlo en el tag productos llamando al funcion makeHtmlTable
+  // Corro makeHtmlTable para generar el html
+  const html = await makeHtmlTable(productos);
+  // pongo el html generado dentro del div productos
+  document.getElementById("productos").innerHTML = html;
 });
 
-function makeHtmlTable(productos) {
-  return fetch('plantillas/tabla-productos.hbs')
+async function makeHtmlTable(productos) {
+  return fetch("plantillas/tabla-productos.hbs")
     .then((respuesta) => respuesta.text())
     .then((plantilla) => {
       const template = Handlebars.compile(plantilla);
@@ -32,74 +37,80 @@ function makeHtmlTable(productos) {
 
 //-------------------------------------------------------------------------------------
 
-// MENSAJES
+const username = document.getElementById("username");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const age = document.getElementById("age");
+const alias = document.getElementById("alias");
+const avatar = document.getElementById("avatar");
+const inputMensaje = document.getElementById("inputMensaje");
 
-/* --------------------- DESNORMALIZACIÓN DE MENSAJES ---------------------------- */
-// Definimos un esquema de autoraaaa
+const btnEnviar = document.getElementById("btnEnviar");
 
-// Definimos un esquema de mensaje
-
-// Definimos un esquema de posts
-
-/* ----------------------------------------------------------------------------- */
-
-const inputUsername = document.getElementById('username');
-const inputMensaje = document.getElementById('inputMensaje');
-const btnEnviar = document.getElementById('btnEnviar');
-
-const formPublicarMensaje = document.getElementById('formPublicarMensaje');
-formPublicarMensaje.addEventListener('submit', (e) => {
+const formPublicarMensaje = document.getElementById("formPublicarMensaje");
+formPublicarMensaje.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  const mensaje = {
+  // creo el objeto y lo envio
+  const message = {
     author: {
-      email: inputUsername.value,
-      nombre: document.getElementById('firstname').value,
-      apellido: document.getElementById('lastname').value,
-      edad: document.getElementById('age').value,
-      alias: document.getElementById('alias').value,
-      avatar: document.getElementById('avatar').value,
+      id: username.value,
+      nombre: firstname.value,
+      apellido: lastname.value,
+      edad: age.value,
+      alias: alias.value,
+      avatar: avatar.value
     },
-    text: inputMensaje.value,
+    text: inputMensaje.value
   };
-
-  socket.emit('nuevoMensaje', mensaje);
+  socket.emit("newMessage", message);
+  //Armar el objeto de mensaje y luego emitir mensaje al evento nuevoMensaje con sockets
   formPublicarMensaje.reset();
   inputMensaje.focus();
 });
 
-socket.on('mensajes', (mensajesN) => {
-  console.log(`Porcentaje de compresión ${porcentajeC}%`);
-  document.getElementById('compresion-info').innerText = porcentajeC;
+socket.on("mensajes", async (normalizedMsgs) => {
+  const initSize = JSON.stringify(normalizedMsgs).length;
+  const denormalizedMensajes = denormalizeData(normalizedMsgs);
 
-  console.log(mensajesD.mensajes);
-  const html = makeHtmlList(mensajesD.mensajes);
-  document.getElementById('mensajes').innerHTML = html;
+  const finalSize = JSON.stringify(denormalizedMensajes).length;
+  const compresion = Math.round((initSize / finalSize) * 100);
+  document.getElementById("compresion-info").innerText = compresion;
+  //generar el html y colocarlo en el tag productos llamando al funcion makeHtmlTable
+  // en este caso cree otra funcion para hacer los mensajes con el formato que pide la consigna
+  const html = await makeHtmlMessages(denormalizedMensajes.mensajes);
+  // cargo ese html generado en el div mensajes
+  document.getElementById("mensajes").innerHTML = html;
 });
 
-function makeHtmlList(mensajes) {
-  return mensajes
-    .map((mensaje) => {
-      return `
-        <div>
-            <b style="color:blue;">${mensaje.author.email}</b>
-            [<span style="color:brown;">${mensaje.fyh}</span>] :
-            <i style="color:green;">${mensaje.text}</i>
-            <img width="50" src="${mensaje.author.avatar}" alt=" ">
-        </div>
-    `;
-    })
-    .join(' ');
+async function makeHtmlMessages(mensajes) {
+  // es la misma funcion que htmlTables, solo que le cambio la plantilla
+  return fetch("plantillas/mensajes.hbs")
+    .then((respuesta) => respuesta.text())
+    .then((plantilla) => {
+      const template = Handlebars.compile(plantilla);
+      const html = template({ mensajes });
+      return html;
+    });
 }
-
-inputUsername.addEventListener('input', () => {
-  const hayEmail = inputUsername.value.length;
-  const hayTexto = inputMensaje.value.length;
-  inputMensaje.disabled = !hayEmail;
-  btnEnviar.disabled = !hayEmail || !hayTexto;
+document.addEventListener("input", () => {
+  if (!username.value || !firstname.value || !lastname.value || !age.value || !alias.value || !avatar.value) {
+    inputMensaje.disabled = true;
+    btnEnviar.disabled = true;
+  } else {
+    inputMensaje.disabled = false;
+    if (!inputMensaje.value) {
+      btnEnviar.disabled = true;
+    } else {
+      btnEnviar.disabled = false;
+    }
+  }
 });
 
-inputMensaje.addEventListener('input', () => {
-  const hayTexto = inputMensaje.value.length;
-  btnEnviar.disabled = !hayTexto;
-});
+const denormalizeData = (normalizedData) => {
+  const schema = normalizr.schema;
+  const author = new schema.Entity("author", { idAttribute: "email" });
+  const mensaje = new schema.Entity("mensaje", { author: author });
+  const mensajes = new schema.Entity("mensajes", { mensajes: [mensaje] });
+  const denormalize = normalizr.denormalize;
+  return denormalize(normalizedData.result, mensajes, normalizedData.entities);
+};
--- src/utils/objectUtils.js
+++ src/utils/objectUtils.js
@@ -1,4 +1,18 @@
-export const returnPlainObj = (obj) => JSON.parse(JSON.stringify(obj));
+<<<<<<< HEAD
+export const returnPlainObj = obj => JSON.parse(JSON.stringify(obj))
+
+export const renameField = (record, from, to) => {
+    record[to] = record[from]
+    delete record[from]
+    return record
+}
+export const removeField = (record, field) => {
+    const value = record[field]
+    delete record[field]
+    return value
+}
+=======
+export const asPOJO = (obj) => JSON.parse(JSON.stringify(obj));
 
 export const renameField = (record, from, to) => {
   record[to] = record[from];
@@ -10,3 +24,4 @@ export const removeField = (record, field) => {
   delete record[field];
   return value;
 };
+>>>>>>> aac81ac4188a13b01de89c6bd4aa83e6bbfb8663
